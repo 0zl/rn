187c5c4844dc516e66f1e86e789aea76ef93e68d
0zl <zwiefk0@gmail.com>
2023-11-22 22:02:25 +0700

    convert to animated webp

--- libs/utils.ts
+++ libs/utils.ts
@@ -1,4 +1,5 @@
 import { tmpdir } from 'os'
+import ffmpeg from 'fluent-ffmpeg'
 
 function randomString(length = 7) {
     return Math.random().toString(36).slice(-length)
@@ -8,7 +9,31 @@ function allocOutFile(format = 'mp4') {
     return `${tmpdir()}/rn_${randomString()}.${format}`
 }
 
+function convertMp4ToWebp(path: string, loop = 0) {
+    return new Promise((res, rej) => {
+        const outFile = `${path}.webp`
+
+        ffmpeg()
+            .input(path)
+            .output(outFile)
+            .on('end', () => {
+                res(outFile)
+            })
+            .on('error', (err: any) => {
+                console.error(err)
+                rej(err)
+            })
+            .videoCodec('libwebp')
+            .native()
+            .loop(loop)
+            .noAudio()
+            .inputOptions('-lossless 1')
+            .run()
+    })
+}
+
 export {
     randomString,
-    allocOutFile
+    allocOutFile,
+    convertMp4ToWebp
 }
